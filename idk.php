62427e511231ed4cbba520041d3c943d67526f85
Odesieus <odesieusPGM@gmail.com>
2014-03-13 01:55:03 -0400

    Create idk.php

---
+++ idk.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Fuck Shit Piss</title>
+		<!-- fuck meta -->
+	</head>
+	<body>
+		<?php
+			$myName = "Coleman";
+			echo "Hi, my name is " .  $myName
+		?>
+	</body>
